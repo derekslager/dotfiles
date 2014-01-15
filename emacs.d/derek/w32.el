073a712bf2ab8807c10328ea6304f5da3bd3cfce
Derek Slager <derekslager@gmail.com>
2014-01-14 18:14:43 -0800

    Adding hack for Symantec Endpoint Protection issue.

--- emacs.d/derek/w32.el
+++ emacs.d/derek/w32.el
@@ -6,3 +6,7 @@
 	    "-outline-Consolas-normal-r-normal-normal-17-102-120-120-c-*-iso8859-1"
 	  "-outline-Consolas-normal-r-normal-normal-13-97-96-96-c-*-iso8859-1"))
   (modify-all-frames-parameters (list (cons 'font font-name))))
+
+;; when using a machine with symantec endpoint protection, this can
+;; prevent horrible slowdowns
+(setq w32-get-true-file-attributes nil)

576f978c7c558214a8cc920e9a4b292c25052a02
Derek Slager <derekslager@gmail.com>
2014-11-24 23:26:07 -0800

    Making company mode completion UI palatable.

--- emacs.d/derek/theme.el
+++ emacs.d/derek/theme.el
@@ -7,3 +7,16 @@
   (let* ((themes (custom-available-themes))
 	 (theme (some (lambda (x) (memq x themes)) theme-preferences)))
     (when theme (load-theme (car theme)))))
+
+(when (package-installed-p 'company)
+  (require 'color)
+  (let ((bg (face-attribute 'default :background)))
+    (custom-set-faces
+     `(company-tooltip ((t (:inherit default :background ,(color-lighten-name bg 2)))))
+     `(company-scrollbar-bg ((t (:background ,(color-lighten-name bg 10)))))
+     `(company-scrollbar-fg ((t (:background ,(color-lighten-name bg 5)))))
+     `(company-tooltip-selection ((t (:inherit font-lock-function-name-face))))
+     `(company-tooltip-common ((t (:inherit font-lock-constant-face))))
+     `(company-tooltip-common-selection ((t (:inherit font-lock-function-name-face))))
+     `(company-preview-common ((t (:inherit font-lock-function-name-face))))
+     `(company-preview-search ((t (:background ,(color-lighten-name bg 2))))))))

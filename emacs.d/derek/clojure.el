603cb780af6a0694897bfb9b0144a4ab1385f5bd
Derek Slager <derekslager@gmail.com>
2014-11-25 11:39:27 -0800

    Adding lein-try helper.

--- emacs.d/derek/clojure.el
+++ emacs.d/derek/clojure.el
@@ -10,3 +10,9 @@
   '(add-to-list 'ac-modes 'cider-mode))
 
 (add-hook 'clojure-mode-hook 'on-clojure-mode)
+
+(defvar lein-try-history nil)
+(defun lein-try (lib-name)
+  (interactive (list (read-string "What are we going to try today?: " nil 'lein-try-history)))
+  (switch-to-buffer (concat "*lein-try-" lib-name "*"))
+  (inferior-lisp (concat "bash lein try " lib-name)))

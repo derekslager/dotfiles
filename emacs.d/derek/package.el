9318ea1d936587789596aef8c0266b317512f9c4
Derek Slager <derekslager@gmail.com>
2015-02-28 17:58:24 -0800

    Trying out a mix of melpa and melpa-stable with pinning for unreliable melpa packages.

--- emacs.d/derek/package.el
+++ emacs.d/derek/package.el
@@ -1,6 +1,12 @@
 (require 'package)
+
 (add-to-list 'package-archives
              '("marmalade" . "http://marmalade-repo.org/packages/") t)
 (add-to-list 'package-archives
              '("melpa-stable" . "http://melpa-stable.milkbox.net/packages/") t)
+(add-to-list 'package-archives
+             '("melpa" . "http://melpa.milkbox.net/packages/") t)
+
+(add-to-list 'package-pinned-packages '(cider . "melpa-stable") t)
+
 (package-initialize)

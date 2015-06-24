4e3a3a4f4a4e3a5bffdd833f1843417c7f8798b6
Derek Slager <derekslager@gmail.com>
2015-06-24 12:48:55 -0700

    Updating profile libs.

--- srv/salt/clojure/profiles.clj
+++ srv/salt/clojure/profiles.clj
@@ -1,5 +1,6 @@
 {:user {:plugins [[jonase/eastwood "0.2.1"]
                   [lein-ancient "0.6.5"]
                   [lein-try "0.4.3"]
-                  [cider/cider-nrepl "0.8.2"]
-                  [refactor-nrepl "0.2.2"]]}}
+                  [cider/cider-nrepl "0.9.1"]
+                  [refactor-nrepl "1.1.0-SNAPSHOT"]]
+        :dependencies [[org.clojure/tools.nrepl "0.2.10"]]}}

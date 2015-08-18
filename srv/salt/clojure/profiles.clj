a0d67b6504628c350e8ae14150760b5821206a7f
Derek Slager <derekslager@gmail.com>
2015-08-18 15:59:59 -0700

    Version updates.

--- srv/salt/clojure/profiles.clj
+++ srv/salt/clojure/profiles.clj
@@ -1,6 +1,6 @@
 {:user {:plugins [[jonase/eastwood "0.2.1"]
-                  [lein-ancient "0.6.5"]
+                  [lein-ancient "0.6.7"]
                   [lein-try "0.4.3"]
                   [cider/cider-nrepl "0.9.1"]
-                  [refactor-nrepl "1.1.0-SNAPSHOT"]]
+                  [refactor-nrepl "1.2.0-SNAPSHOT"]]
         :dependencies [[org.clojure/tools.nrepl "0.2.10"]]}}

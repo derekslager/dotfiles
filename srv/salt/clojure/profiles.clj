d733eeb6424a611a22673b407f10100c669d60e9
Derek Slager <derekslager@gmail.com>
2016-01-14 23:19:13 -0800

    Adding lein-repo.

--- srv/salt/clojure/profiles.clj
+++ srv/salt/clojure/profiles.clj
@@ -1,5 +1,6 @@
 {:user {:plugins [[jonase/eastwood "0.2.3"]
                   [lein-ancient "0.6.8"]
+                  [lein-repo "0.2.1"]
                   [lein-try "0.4.3"]
                   [cider/cider-nrepl "0.11.0-SNAPSHOT"]
                   [refactor-nrepl "2.0.0-SNAPSHOT"]]

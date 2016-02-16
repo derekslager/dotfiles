e80120310678a6dcbd3a1c8bb8f71fedc3171158
Derek Slager <derekslager@gmail.com>
2016-02-16 09:10:20 -0800

    Bump version of refactor middleware.

--- srv/salt/clojure/profiles.clj
+++ srv/salt/clojure/profiles.clj
@@ -3,5 +3,5 @@
                   [lein-repo "0.2.1"]
                   [lein-try "0.4.3"]
                   [cider/cider-nrepl "0.11.0-SNAPSHOT"]
-                  [refactor-nrepl "2.0.0-SNAPSHOT"]]
+                  [refactor-nrepl "2.2.0-SNAPSHOT"]]
         :dependencies [[org.clojure/tools.nrepl "0.2.12"]]}}

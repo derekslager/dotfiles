ba38b8d08d22007c77b8f28cf7feb0a4162a4a23
Derek Slager <derekslager@gmail.com>
2016-02-16 23:20:43 -0800

    Re-pin CIDER, clj-refactor.

--- srv/salt/clojure/profiles.clj
+++ srv/salt/clojure/profiles.clj
@@ -2,6 +2,6 @@
                   [lein-ancient "0.6.8"]
                   [lein-repo "0.2.1"]
                   [lein-try "0.4.3"]
-                  [cider/cider-nrepl "0.11.0-SNAPSHOT"]
-                  [refactor-nrepl "2.2.0-SNAPSHOT"]]
+                  [cider/cider-nrepl "0.10.2"]
+                  [refactor-nrepl "2.0.0"]]
         :dependencies [[org.clojure/tools.nrepl "0.2.12"]]}}

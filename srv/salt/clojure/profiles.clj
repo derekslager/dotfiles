e54d66ca1c8cd5bff3a0ae0d5ac9ce67b201a243
Derek Slager <derekslager@gmail.com>
2016-02-21 10:29:47 -0800

    Adding lein-repo to :uberjar profile.

--- srv/salt/clojure/profiles.clj
+++ srv/salt/clojure/profiles.clj
@@ -4,4 +4,5 @@
                   [lein-try "0.4.3"]
                   [cider/cider-nrepl "0.10.2"]
                   [refactor-nrepl "2.0.0"]]
-        :dependencies [[org.clojure/tools.nrepl "0.2.12"]]}}
+        :dependencies [[org.clojure/tools.nrepl "0.2.12"]]}
+ :uberjar {:plugins [[lein-repo "0.2.1"]]}}

153643cc763432d035da90628e30ecb81731af34
Derek Slager <derekslager@gmail.com>
2016-03-03 10:21:13 -0800

    Dropping SNAPSHOT from CIDER.

--- srv/salt/clojure/profiles.clj
+++ srv/salt/clojure/profiles.clj
@@ -2,7 +2,7 @@
                   [lein-ancient "0.6.8"]
                   [lein-repo "0.2.1"]
                   [lein-try "0.4.3"]
-                  [cider/cider-nrepl "0.11.0-SNAPSHOT"]
+                  [cider/cider-nrepl "0.11.0"]
                   [refactor-nrepl "2.2.0-SNAPSHOT"]]
         :dependencies [[org.clojure/tools.nrepl "0.2.12"]]}
  :uberjar {:plugins [[lein-repo "0.2.1"]]}}

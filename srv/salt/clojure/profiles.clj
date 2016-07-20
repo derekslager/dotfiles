28c4b02ae13cb9cbb2045944411402c696dd2d73
Derek Slager <derekslager@gmail.com>
2016-07-20 15:50:49 -0700

    Update lein-monolith.

--- srv/salt/clojure/profiles.clj
+++ srv/salt/clojure/profiles.clj
@@ -1,6 +1,6 @@
 {:user {:plugins [[acyclic/squiggly-clojure "0.1.5"]
                   [jonase/eastwood "0.2.3"]
                   [lein-ancient "0.6.10"]
-                  [lein-monolith "0.1.0-SNAPSHOT"]
+                  [lein-monolith "0.2.0"]
                   [lein-pprint "1.1.2"]
                   [lein-try "0.4.3"]]}}

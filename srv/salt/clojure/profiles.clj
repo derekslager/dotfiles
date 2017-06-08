8cd41f350358e6025c6062b670dba12f8125ddac
Derek Slager <derekslager@gmail.com>
2017-06-08 13:43:54 -0700

    Lib updates.

--- srv/salt/clojure/profiles.clj
+++ srv/salt/clojure/profiles.clj
@@ -1,8 +1,8 @@
-{:user {:plugins [[acyclic/squiggly-clojure "0.1.6"]
-                  [jonase/eastwood "0.2.3"]
+{:user {:plugins [[acyclic/squiggly-clojure "0.1.8" :exclusions [org.clojure/clojure]]
+                  [jonase/eastwood "0.2.4" :exclusions [org.clojure/clojure]]
                   [lein-ancient "0.6.10"]
-                  [lein-monolith "0.3.0"]
+                  [lein-monolith "1.0.1"]
                   [lein-pprint "1.1.2"]
                   [lein-try "0.4.3"]]}
- :my/repl ^:repl {:plugins [[cider/cider-nrepl "0.13.0"]
+ :my/repl ^:repl {:plugins [[cider/cider-nrepl "0.14.0"]
                             [refactor-nrepl "2.2.0"]]}}

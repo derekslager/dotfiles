bd92a07a17f2300d3caa466841c9de9d0ed7e3c5
Derek Slager <derekslager@gmail.com>
2020-10-16 15:20:38 -0700

    Version upgrades.

--- srv/salt/clojure/profiles.clj
+++ srv/salt/clojure/profiles.clj
@@ -1,8 +1,10 @@
-{:user {:plugins [[jonase/eastwood "0.2.9" :exclusions [org.clojure/clojure]]
+{:user {:plugins [[jonase/eastwood "0.3.11" :exclusions [org.clojure/clojure]]
                   [lein-ancient "0.6.15"]
-                  [lein-monolith "1.1.0"]
-                  [lein-pprint "1.2.0"]
-                  [lein-try "0.4.3"]]}
+                  [lein-monolith "1.5.0"]
+                  [lein-pprint "1.3.2"]
+                  [lein-try "0.4.3"]
+                  #_[lein-nvd "1.0.0"]
+                  [lein-assoc "0.1.0"]]}
  ;; :my/repl ^:repl {:plugins [[cider/cider-nrepl "0.17.0"]
  ;;                            [refactor-nrepl "2.3.1"]
  ;;                            ]}
